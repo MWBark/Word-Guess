3c5aa1f42e1ae1c436489afd35dd5f364231868b
Michael Barker <michael.w.barker@hotmail.co.uk>
2024-04-20 12:40:48 +0000

    Create 'get_input' function and 'check_guess' function to check input

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ print(word)
 def create_empty_list():
     """
     Creates a number of underscores equal to the random word length
-    that will be updated with letters the user guesses
+    that will be updated with letters the user guesses.
     """
     global empty_list
     empty_list =[]
@@ -24,4 +24,42 @@ def create_empty_list():
     print(*empty_list)
     return empty_list
 
+
+def get_input():
+    """
+    Gets user's letter or word guess.
+    """
+    global user_guess
+    user_guess = input("Enter a letter or word: \n")
+    return user_guess
+
+
+def check_guess():
+    """
+    First checks user's guess is a letter
+    then updates the 'empty_list' if it is, 
+    then test wether the user's guess or 'empty_string'
+    matches the random word.
+    """
+    global doesnt_contain, lives, currentscore
+    if len(user_guess) < 2:       
+        if user_guess not in [*word]:
+            doesnt_contain += user_guess
+        else:
+            for i in range(len([*word])):
+                if [*word][i] == user_guess:
+                    empty_list[i] = user_guess
+        
+
+    if empty_list == [*word] or user_guess == word:
+        print(f"Congrats! The word was {word}")
+    else:
+        print(*empty_list)
+        get_input()
+        check_guess()
+
+
+
 create_empty_list()
+get_input()
+check_guess()
