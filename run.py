d65b894d2d8450e35fb33296940aa5e012412cce
Michael Barker <michael.w.barker@hotmail.co.uk>
2024-04-20 11:54:59 +0000

    Add import

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random_word import Wordnik
+
+wordnik_service = Wordnik()
+
+word = wordnik_service.get_random_word()
+
+print(word)
