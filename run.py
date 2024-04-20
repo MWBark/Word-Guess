3972da824d5d1743db7a38de23a17a6c2559d951
Michael Barker <michael.w.barker@hotmail.co.uk>
2024-04-20 12:20:25 +0000

    Create 'create_empty_list' function

--- run.py
+++ run.py
@@ -5,3 +5,23 @@ wordnik_service = Wordnik()
 word = wordnik_service.get_random_word()
 
 print(word)
+
+def create_empty_list():
+    """
+    Creates a number of underscores equal to the random word length
+    that will be updated with letters the user guesses
+    """
+    global empty_list
+    empty_list =[]
+
+    for i in [*word]:
+        if i == '-':
+            empty_list += '-'
+        else:
+            empty_list += "_"
+
+    print(f"{len(word)} letters")
+    print(*empty_list)
+    return empty_list
+
+create_empty_list()
