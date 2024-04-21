512bda35958bb08647e24f4d9c98f14922ab31f2
Michael Barker <michael.w.barker@hotmail.co.uk>
2024-04-21 08:58:57 +0000

    Add 'lives' feature. Create 'check_stats' function to make decision based on 'lives' value

--- run.py
+++ run.py
@@ -2,14 +2,16 @@ from random_word import Wordnik
 
 wordnik_service = Wordnik()
 doesnt_contain = []
+lives = 0
 
 
 def new_word():
     """
     Gets a new random word.
     """
-    global word
+    global word, lives
     word = wordnik_service.get_random_word().lower()
+    lives += len(word)
     print(word)
     return word
 
@@ -32,10 +34,11 @@ def create_empty_list():
 
 def display_info():
     """
-    Displays word length and letters the word doesn't contain to help user guess
+    Displays word length, letters the word doesn't contain
+    and lives to help user guess
     """
     letters_string = ", ".join(doesnt_contain)
-    print(f"\n{len(word)} letters, doesn't contain: {letters_string}")
+    print(f"\nlives: {lives}\n{len(word)} letters, doesn't contain: {letters_string}")
     print(*empty_list)
 
 
@@ -48,6 +51,22 @@ def get_input():
     return user_guess
 
 
+def check_stats():
+    """
+    Check the user's stats, such as lives.
+    Runs a new game or new guess based on lives value.
+    """
+    global doesnt_contain
+    if lives == 0:
+        print(f"\nBad luck. the word was {word}")
+        doesnt_contain = []
+        run_game()
+    else:
+        display_info()
+        get_input()
+        check_guess()
+
+
 def check_guess():
     """
     First checks user's guess is a letter
@@ -55,8 +74,8 @@ def check_guess():
     then test wether the user's guess or 'empty_string'
     matches the random word.
     """
-    if len(user_guess) < 2:   
-        global doesnt_contain    
+    global doesnt_contain, lives
+    if len(user_guess) < 2:       
         if len(user_guess) < 2:       
             if user_guess not in [*word]:
                 doesnt_contain += user_guess
@@ -70,9 +89,8 @@ def check_guess():
         print(f"Congrats! The word was {word}")
         run_game()
     else:
-        display_info()
-        get_input()
-        check_guess()
+        lives -= 1
+        check_stats()
 
 
 def run_game():
